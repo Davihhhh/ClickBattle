2798f5d6e7f79701cf2d1fbe012dc3f512b79502
Davihhhh <101269407+Davihhhh@users.noreply.github.com>
2024-03-17 00:56:28 +0100

    Add files via upload

---
+++ ServerCB/ServerCB/Giocatore.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCB
+{
+    public class Giocatore
+    {
+        private string _nome;
+        private int _punteggioMassimo, _vittorie;
+
+        public string Nome 
+        { 
+            get { return _nome; } 
+            private set { _nome = value; } 
+        }
+        public int PunteggioMassimo
+        {
+            get { return _punteggioMassimo; }
+            private set { _punteggioMassimo = value; }
+        }
+        public int Vittorie
+        {
+            get { return _vittorie; }
+            private set { _vittorie = value; }
+        }
+
+        public Giocatore(string nome, int punteggioMassimo, int vittorie)
+        {
+            Nome = nome;
+            PunteggioMassimo = punteggioMassimo;
+            Vittorie = vittorie;
+        }
+        
+        public void AggiungiVittoria(int punteggio)
+        {
+            Vittorie += 1;
+            if (PunteggioMassimo < punteggio)
+            {
+                PunteggioMassimo = punteggio;
+            }
+        }
+    }
+}
