79e775d048c57514a7a1e5a22e0b4c254ae81425
Davide Lin <davidelin2005@gmail.com>
2024-03-17 00:41:52 +0100

    Debug start

--- ClientCB/ClientCB/Form1.Designer.cs
+++ ClientCB/ClientCB/Form1.Designer.cs
@@ -37,12 +37,17 @@
             this.timerPartita = new System.Windows.Forms.Timer(this.components);
             this.button = new System.Windows.Forms.Button();
             this.buttonRivincita = new System.Windows.Forms.Button();
+            this.buttonPartita = new System.Windows.Forms.Button();
+            this.buttonCancella = new System.Windows.Forms.Button();
+            this.labelRecord = new System.Windows.Forms.Label();
+            this.labelVittorie = new System.Windows.Forms.Label();
+            this.labelNome = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonGioca
             // 
             this.buttonGioca.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
-            this.buttonGioca.Location = new System.Drawing.Point(573, 339);
+            this.buttonGioca.Location = new System.Drawing.Point(609, 316);
             this.buttonGioca.Name = "buttonGioca";
             this.buttonGioca.Size = new System.Drawing.Size(101, 57);
             this.buttonGioca.TabIndex = 0;
@@ -53,7 +58,7 @@
             // textBoxNome
             // 
             this.textBoxNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
-            this.textBoxNome.Location = new System.Drawing.Point(509, 275);
+            this.textBoxNome.Location = new System.Drawing.Point(545, 252);
             this.textBoxNome.Name = "textBoxNome";
             this.textBoxNome.Size = new System.Drawing.Size(236, 31);
             this.textBoxNome.TabIndex = 1;
@@ -62,7 +67,7 @@
             // 
             this.labelAttesa.AutoSize = true;
             this.labelAttesa.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
-            this.labelAttesa.Location = new System.Drawing.Point(469, 209);
+            this.labelAttesa.Location = new System.Drawing.Point(505, 186);
             this.labelAttesa.Name = "labelAttesa";
             this.labelAttesa.Size = new System.Drawing.Size(314, 31);
             this.labelAttesa.TabIndex = 2;
@@ -71,7 +76,7 @@
             // 
             // buttonLogout
             // 
-            this.buttonLogout.Location = new System.Drawing.Point(1431, 12);
+            this.buttonLogout.Location = new System.Drawing.Point(1223, 12);
             this.buttonLogout.Name = "buttonLogout";
             this.buttonLogout.Size = new System.Drawing.Size(75, 23);
             this.buttonLogout.TabIndex = 3;
@@ -93,28 +98,90 @@
             // button
             // 
             this.button.BackColor = System.Drawing.Color.LimeGreen;
-            this.button.Location = new System.Drawing.Point(275, 423);
+            this.button.Location = new System.Drawing.Point(311, 400);
             this.button.Name = "button";
             this.button.Size = new System.Drawing.Size(50, 50);
             this.button.TabIndex = 4;
             this.button.UseVisualStyleBackColor = false;
             this.button.Visible = false;
+            this.button.Click += new System.EventHandler(this.button_Click);
             // 
             // buttonRivincita
             // 
             this.buttonRivincita.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
-            this.buttonRivincita.Location = new System.Drawing.Point(558, 423);
+            this.buttonRivincita.Location = new System.Drawing.Point(594, 400);
             this.buttonRivincita.Name = "buttonRivincita";
             this.buttonRivincita.Size = new System.Drawing.Size(127, 38);
             this.buttonRivincita.TabIndex = 5;
             this.buttonRivincita.Text = "Rivincita";
             this.buttonRivincita.UseVisualStyleBackColor = true;
+            this.buttonRivincita.Visible = false;
+            this.buttonRivincita.Click += new System.EventHandler(this.buttonRivincita_Click);
+            // 
+            // buttonPartita
+            // 
+            this.buttonPartita.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.buttonPartita.Location = new System.Drawing.Point(594, 410);
+            this.buttonPartita.Name = "buttonPartita";
+            this.buttonPartita.Size = new System.Drawing.Size(127, 67);
+            this.buttonPartita.TabIndex = 6;
+            this.buttonPartita.Text = "Cerca Avversario";
+            this.buttonPartita.UseVisualStyleBackColor = true;
+            this.buttonPartita.Visible = false;
+            this.buttonPartita.Click += new System.EventHandler(this.buttonPartita_Click);
+            // 
+            // buttonCancella
+            // 
+            this.buttonCancella.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.buttonCancella.Location = new System.Drawing.Point(594, 234);
+            this.buttonCancella.Name = "buttonCancella";
+            this.buttonCancella.Size = new System.Drawing.Size(127, 67);
+            this.buttonCancella.TabIndex = 7;
+            this.buttonCancella.Text = "Cancella";
+            this.buttonCancella.UseVisualStyleBackColor = true;
+            this.buttonCancella.Visible = false;
+            this.buttonCancella.Click += new System.EventHandler(this.buttonCancella_Click);
+            // 
+            // labelRecord
+            // 
+            this.labelRecord.AutoSize = true;
+            this.labelRecord.Location = new System.Drawing.Point(12, 32);
+            this.labelRecord.Name = "labelRecord";
+            this.labelRecord.Size = new System.Drawing.Size(42, 13);
+            this.labelRecord.TabIndex = 8;
+            this.labelRecord.Text = "Record";
+            this.labelRecord.Visible = false;
+            // 
+            // labelVittorie
+            // 
+            this.labelVittorie.AutoSize = true;
+            this.labelVittorie.Location = new System.Drawing.Point(12, 56);
+            this.labelVittorie.Name = "labelVittorie";
+            this.labelVittorie.Size = new System.Drawing.Size(39, 13);
+            this.labelVittorie.TabIndex = 10;
+            this.labelVittorie.Text = "Vittorie";
+            this.labelVittorie.Visible = false;
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(12, 9);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(35, 13);
+            this.labelNome.TabIndex = 11;
+            this.labelNome.Text = "Nome";
+            this.labelNome.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1518, 650);
+            this.ClientSize = new System.Drawing.Size(1306, 644);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.labelVittorie);
+            this.Controls.Add(this.labelRecord);
+            this.Controls.Add(this.buttonCancella);
+            this.Controls.Add(this.buttonPartita);
             this.Controls.Add(this.buttonRivincita);
             this.Controls.Add(this.button);
             this.Controls.Add(this.buttonLogout);
@@ -139,6 +206,11 @@
         private System.Windows.Forms.Timer timerPartita;
         private System.Windows.Forms.Button button;
         private System.Windows.Forms.Button buttonRivincita;
+        private System.Windows.Forms.Button buttonPartita;
+        private System.Windows.Forms.Button buttonCancella;
+        private System.Windows.Forms.Label labelRecord;
+        private System.Windows.Forms.Label labelVittorie;
+        private System.Windows.Forms.Label labelNome;
     }
 }
 
--- ClientCB/ClientCB/Form1.cs
+++ ClientCB/ClientCB/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Net.Sockets;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,69 +20,55 @@ namespace ClientCB
             InitializeComponent();
         }
 
-        TcpClient client;
-        NetworkStream stream;
-        Timer timer;
-        int elapsedTime;
+        // Variabili Client
+        private TcpClient client;
+        private NetworkStream stream;
+        private int elapsedTime, clicks;
+        private int limSx, limDx, limUp, limDw;
+        private Random random = new Random();
+        private string nome;
+        private byte[] responseData = new byte[1024];
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            elapsedTime = 0;
+            clicks = 0;
+            limSx = 0;
+            limUp = 0;
+            limDx = this.Width;
+            limDw = this.Height;
+        }       
 
+        // Funzioni
+        private void InviaMessaggioServer(string message)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+        }
+        private string RiceviMessaggioServer()
+        {
+            int bytesRead = stream.Read(responseData, 0, responseData.Length);
+            string response = Encoding.ASCII.GetString(responseData, 0, bytesRead);
+            return response;
         }
 
-        private void Login_Click(object sender, EventArgs e)
-        {
-            try
-            {               
-                client = new TcpClient("127.0.0.1", 1000);
-                stream = client.GetStream();
-
-                // Invio delle credenziali al server
-                string username = textBoxNome.Text;
-
-                byte[] data = Encoding.ASCII.GetBytes(username);
-                
-                stream.Write(data, 0, data.Length);
-
-                // Ricezione della risposta dal server (se necessario)
-                // In questo esempio, supponiamo che il server invii una risposta
-                byte[] responseData = new byte[1024];
-                int bytesRead = stream.Read(responseData, 0, responseData.Length);
-                string response = Encoding.ASCII.GetString(responseData, 0, bytesRead);
-
-                // Visualizzazione della risposta
-                if (response == "ok")
-                {
-                    Wait(client, stream);                  
-                }
-                else
-                {
-                    MessageBox.Show("Error");
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Errore: " + ex.Message);
-            }
-        }
-        private void Wait(TcpClient client, NetworkStream stream)
-        {
-            textBoxNome.Visible = false;
-            buttonGioca.Visible = false;   
-
+        // Richieste al Server
+        private void Wait()
+        {       
             labelAttesa.Visible = true;
 
-            byte[] responseData = new byte[1024];
-            int bytesRead;
+            string message = nome + ";sfida";
+
+            InviaMessaggioServer(message);
+             
             string response;
 
             while (true)
             {
-                bytesRead = stream.Read(responseData, 0, responseData.Length);
-                response = Encoding.ASCII.GetString(responseData, 0, bytesRead);
-
+                response = RiceviMessaggioServer();
                 if (response == "gioca")
                 {
+                    labelAttesa.Visible = false;
                     Gioca();
                 }
                 else
@@ -90,7 +77,6 @@ namespace ClientCB
                 }
             }
         }
-
         private void Gioca()
         {
             InizioPartita();
@@ -102,39 +88,151 @@ namespace ClientCB
         }
         private void InizioPartita()
         {
+            button.Visible = true;
+            buttonLogout.Visible = false;
             timerPartita.Start();
             timerSposta.Start();
             elapsedTime = 0;
+            clicks = 0;
+            labelNome.Visible = false;
+            labelRecord.Visible = false;
+            labelVittorie.Visible = false;
         }
         private void FinePartita()
         {            
             button.Visible = false;
-            buttonLogout.Visible = false;
+            buttonLogout.Visible = true;
+            labelNome.Visible = true;
+            labelRecord.Visible = true;
+            labelVittorie.Visible = true;
             timerSposta.Stop();
             timerPartita.Stop();
+            InviaPunteggio();
         }
-
         private void Logout(TcpClient client, NetworkStream stream)
         {
-            stream.Close();
-            client.Close();
+            try
+            {
+                string message = nome + ";disconnetto";
+                InviaMessaggioServer(message);
 
-            textBoxNome.Visible = true;
-            buttonGioca.Visible = true;
+                stream.Close();
+                client.Close();
+
+                textBoxNome.Visible = true;
+                buttonGioca.Visible = true;
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+
         }
-        private void timerSposta_Tick(object sender, EventArgs e)
+        private void InviaPunteggio()
         {
-            
-        }
+            try
+            {
+                string message = nome + ';' + clicks;
 
-        private void timerPartita_Tick(object sender, EventArgs e)
+                InviaMessaggioServer(message);
+                
+                string response = RiceviMessaggioServer();
+
+                if (response == "fine")
+                {
+                    buttonPartita.Visible = true;
+                    buttonCancella.Visible = false;
+                    labelAttesa.Visible = false;
+                }
+            } catch(Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        private void CancellaRichiesta()
         {
-            elapsedTime++;
+            string message = nome + ";cancella";
+
+            InviaMessaggioServer(message);
+
+            string response = RiceviMessaggioServer();
+
+            if (response == "cancellato")
+            {
+                buttonPartita.Visible = true;
+                buttonCancella.Visible = false;
+                labelAttesa.Visible = false;
+            }             
         }
 
+        // Button Clicks
+        private void Login_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new TcpClient("127.0.0.1", 10000);
+                stream = client.GetStream();
+
+                nome = textBoxNome.Text;
+
+                // Invio delle credenziali al server
+                string message = nome + ";login";
+
+                InviaMessaggioServer(message);
+
+                int bytesRead = stream.Read(responseData, 0, responseData.Length);
+                string response = Encoding.ASCII.GetString(responseData, 0, bytesRead);
+                string[] msg = response.Split(';');
+                // Visualizzazione della risposta
+                if (msg[0] == "loggato")
+                {
+                    buttonPartita.Visible = true;
+                    textBoxNome.Visible = false;
+                    buttonGioca.Visible = false;
+
+                    labelNome.Visible = true;
+                    labelNome.Text = "Nome: " + nome;
+                    labelRecord.Visible = true;
+                    labelRecord.Text = "Record: " + msg[1];
+                    labelVittorie.Visible = true;
+                    labelVittorie.Text = "Vittorie: " + msg[2];
+                }
+                else
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore: " + ex.Message);
+            }
+        }
         private void buttonLogout_Click(object sender, EventArgs e)
         {
             Logout(client, stream);
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            clicks++;
+        }
+        private void buttonPartita_Click(object sender, EventArgs e)
+        {
+            Wait();
+        }
+        private void buttonCancella_Click(object sender, EventArgs e)
+        {
+            CancellaRichiesta();
+        }
+        private void buttonRivincita_Click(object sender, EventArgs e)
+        {
+            Wait();
+        }
+
+        // Timers
+        private void timerSposta_Tick(object sender, EventArgs e)
+        {
+            int x, y;
+            x = random.Next(limSx, limDx);
+            y = random.Next(limUp, limDw);
+            button.Location = new Point(x, y);
+        }
+        private void timerPartita_Tick(object sender, EventArgs e)
+        {
+            elapsedTime++;
         }
     }
 }
