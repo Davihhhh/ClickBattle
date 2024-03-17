5d50d71e2f77f84d83f0a282e5486edb1b6dd911
Davihhhh <101269407+Davihhhh@users.noreply.github.com>
2024-03-17 22:01:52 +0100

    Add files via upload

---
+++ ServerCB/ServerCB/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace ServerCB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.BackColor = System.Drawing.Color.Tomato;
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.buttonStop.Location = new System.Drawing.Point(429, 83);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(153, 69);
+            this.buttonStop.TabIndex = 0;
+            this.buttonStop.Text = "Stop Server";
+            this.buttonStop.UseVisualStyleBackColor = false;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.Color.LightGreen;
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.buttonStart.Location = new System.Drawing.Point(179, 83);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(153, 69);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start Server";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 262);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonStop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ ServerCB/ServerCB/Partita.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCB
+{
+    public class Partita
+    {
+        private string _nome1, _nome2;
+        private int _punteggio1, _punteggio2;
+        private DateTime _data;
+
+        public string Nome1
+        {
+            get { return _nome1; }
+            private set { _nome1 = value; }
+        }
+        public string Nome2
+        {
+            get { return _nome2; }
+            private set { _nome2 = value; }
+        }
+        public int Punteggio1
+        {
+            get { return _punteggio1; }
+            private set { _punteggio1 = value; }
+        }
+        public int Punteggio2
+        {
+            get { return _punteggio2; }
+            private set { _punteggio2 = value; }
+        }
+        public DateTime Data
+        {
+            get { return _data; }
+            private set { _data = value; }
+        }
+
+        public Partita(string nome1, string nome2, int punteggio1, int punteggio2) 
+        {
+            Nome1 = nome1;
+            Nome2 = nome2;
+            Punteggio1 = punteggio1;
+            Punteggio2 = punteggio2;
+            _data = DateTime.Now;
+        }
+
+    }
+}
